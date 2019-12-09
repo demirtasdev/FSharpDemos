22d4508ac4a2059ae622c6d28683fb23aff83154
d3m1rt45 <d3m1rt45@outlook.com>
2019-12-09 00:41:57 +0000

    Small notes added as comments.

--- YouTube/FSharpInOneVideo/Program.fs
+++ YouTube/FSharpInOneVideo/Program.fs
@@ -1,13 +1,15 @@
 ﻿open System
 
 
+// takes two int and returns their sum
 let sum (x:int, y:int) : int = x + y
 
+// a recursive function keeps calling itself until it returns a value
 let rec factorial x =
+    // in this case it returns a value when x is smaller than 1
     if x < 1 then 1
+    // ..and until then it keeps calling itself with x's previous value - 1
     else x * factorial (x - 1)
-
-
 
 [<EntryPoint>]
 let main argv =
@@ -18,6 +20,6 @@ let main argv =
     // `factorial` function call
     printfn "Factorial 4 : %i" (factorial 4)
 
-
+    // keep the console open
     Console.ReadKey() |> ignore
     0 // return an integer exit code
