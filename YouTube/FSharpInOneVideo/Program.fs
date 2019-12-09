e7754da9443855850dbdcdf74ad0634c14c2753a
d3m1rt45 <d3m1rt45@outlook.com>
2019-12-09 17:56:28 +0000

    FilterAndMultiply
    I've written a block of code using the pipeline operator that filters a list of integers using List.filter function depending on the criteria that is defined in the lambda expression that we pass to it.

--- YouTube/FSharpInOneVideo/Program.fs
+++ YouTube/FSharpInOneVideo/Program.fs
@@ -1,9 +1,11 @@
 ﻿open System
 
-
+// 01_FIRSTFUNCTION:
 // takes two int and returns their sum
 let sum (x:int, y:int) : int = x + y
 
+
+// 02_RECURSIVEFUNCTIONS:
 // a recursive function keeps calling itself until it returns a value
 let rec factorial x =
     // in this case it returns a value when x is smaller than 1
@@ -18,11 +20,29 @@ let rec factorial x =
         // 4th : result = 1 * factorial(0) = 1 * 1 = 1
 
 
+// 03_LAMBDAEXPRESSIONS
+// Create a list of two integers
+let randList = [1;2;3]
+// Use a lambda expression to multiply each value by 2 (notice "fun x ->..."):
+let randListTwo = List.map (fun x -> x * 2) randList
+// map function that exists in the List module maps through each item of the list
+
+
+// Lambda function using the pipeline operator
+let filterAndMultiply() =
+    [5;6;7;8]
+    // List.filter returns a new list containing only the numbers that meets the expression's criteria
+    |> List.filter (fun v -> (v % 2) = 0)
+    // Then we can map through the list as before and multiply each value by 2
+    |> List.map (fun x -> x * 2)
+    // And then print the values:
+    |> printfn "Even Doubles: %A"
+
+
 [<EntryPoint>]
 let main argv =
 
-    // `factorial` function call
-    printfn "Factorial 4 : %i" (factorial 4)
+    filterAndMultiply()
 
     // keep the console open
     Console.ReadKey() |> ignore
