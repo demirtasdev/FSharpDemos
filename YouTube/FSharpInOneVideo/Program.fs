bfdff3c70b70a22fe51ea6cffa4d16f3dc25eec3
d3m1rt45 <d3m1rt45@outlook.com>
2019-12-09 00:21:58 +0000

    GetSum Function

--- YouTube/FSharpInOneVideo/Program.fs
+++ YouTube/FSharpInOneVideo/Program.fs
@@ -1,14 +1,13 @@
 ﻿open System
 
-module Funcs =
+[<EntryPoint>]
+let main argv =
+
     let doFuncs() =
         let getSum (x:int, y:int) : int = x + y
         printfn "5 + 7 = %i" (getSum(5, 7))
 
-[<EntryPoint>]
-let main argv =
-
-    Funcs.doFuncs()
-    Console.ReadKey |> ignore
+    doFuncs()
+    Console.ReadKey() |> ignore
     
     0 // return an integer exit code
