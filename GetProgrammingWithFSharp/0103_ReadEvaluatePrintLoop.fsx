756e6585c0af67a0e48689991b150a810909fa86
demirtasdev <demirtasdev@outlook.com>
2019-12-16 19:08:55 +0000

    Count Words
    Written the count words method from the "Try This" section of the unit.

--- GetProgrammingWithFSharp/0103_ReadEvaluatePrintLoop.fsx
+++ GetProgrammingWithFSharp/0103_ReadEvaluatePrintLoop.fsx
@@ -1,3 +1,5 @@
+open System;
+
 let text = "Hello, world!"
 
 text.Length
@@ -7,12 +9,17 @@ let greetPerson name age =
 
 let greeting = greetPerson "Fred" 25
 
+// TRY THIS:
+let countWords (text:string) =
+        text.Split(" ")
+        |> Array.length
+
 
 // QUICK CHECK 3.2
 //
-// Q: Do scripts need a project in order to run?
-// A: No - you can run scripts as standalone files.
+// Q1: Do scripts need a project in order to run?
+// A1: No - you can run scripts as standalone files.
 //
-// Q: Give two reasons that you might use a script rather than coding
-//    directly into FSI.
-// A: F# scripts have an improved development experience and are repeatable.
+// Q2: Give two reasons that you might use a script rather than coding
+//     directly into FSI.
+// A3: F# scripts have an improved development experience and are repeatable.
