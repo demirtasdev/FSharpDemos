2bac829f9b967af2f3b79170e21a3559f81ddb91
demirtasdev <demirtasdev@outlook.com>
2019-12-16 18:44:39 +0000

    Added Quick Check Q/As

--- GetProgrammingWithFSharp/0102_FirstFSharpProgram.fs
+++ GetProgrammingWithFSharp/0102_FirstFSharpProgram.fs
@@ -19,3 +19,15 @@ let main argv =
 // %s - string
 // %O - The .ToString() representation of the argument
 // %A - An F# pretty-print representation of the argument that falls back to %O if none exists
+
+
+// QUICK CHECK 2.1:
+//
+// Q1: What are the two basic project types for F# shipped with Visual Studio?
+// A1: Console and Class Library
+//
+// Q2: What is the [<EntryPoint>] attribute for?
+// A2: Marking the entry function to a console application
+//
+// Q3: What are the two types of F# files in a project?
+// A3: .fs (compiled file) and .fsx (script file) 
--- GetProgrammingWithFSharp/0103_ReadEvaluatePrintLoop.fsx
+++ GetProgrammingWithFSharp/0103_ReadEvaluatePrintLoop.fsx
@@ -7,3 +7,12 @@ let greetPerson name age =
 
 let greeting = greetPerson "Fred" 25
 
+
+// QUICK CHECK 3.2
+//
+// Q: Do scripts need a project in order to run?
+// A: No - you can run scripts as standalone files.
+//
+// Q: Give two reasons that you might use a script rather than coding
+//    directly into FSI.
+// A: F# scripts have an improved development experience and are repeatable.
