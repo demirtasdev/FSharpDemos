170189400d41f552e79597d0dab87f93f721cb85
demirtasdev <demirtasdev@outlook.com>
2019-12-23 11:27:31 +0000

    Project Created for Capstone 4 (Unit 5)
    Since there were some things added to the code from the last capstone behind the scenes, I decided that I would just use the exercise files. Since I don't have Visual Studio installed on my machine, I have had to migrate the code from the exercise file into an ionide project.
    
    Now I have the proper basis to go through the fourth Capstone.

---
+++ GetProgrammingWithFSharp/Unit_05/Auditing.fs
@@ -0,0 +1,19 @@
+namespace Capstone4
+
+open Capstone4.Operations
+open Capstone4.Domain
+
+
+module Auditing =
+    /// Logs to the console
+    let printTransaction _ accountId transaction =
+        printfn "Account %O: %s of %M (approved: %b)" accountId transaction.Operation transaction.Amount transaction.Accepted
+
+    // Logs to both console and file system
+    let composedLogger = 
+        let loggers =
+            [ FileRepository.writeTransaction
+              printTransaction ]
+        fun accountId owner transaction ->
+            loggers
+            |> List.iter(fun logger -> logger accountId owner transaction)
---
+++ GetProgrammingWithFSharp/Unit_05/Program.fs
@@ -0,0 +1,61 @@
+﻿namespace Capstone4.Program
+
+open System
+open Capstone4.Domain
+open Capstone4.Operations
+open Capstone4.Auditing
+open Capstone4.FileRepository
+
+[<AutoOpen>]
+module CommandParsing =
+    let isValidCommand cmd = [ 'd'; 'w'; 'x' ] |> List.contains cmd
+    let isStopCommand = (=) 'x'
+
+[<AutoOpen>]
+module UserInput =
+    let commands = seq {
+        while true do
+            Console.Write "(d)eposit, (w)ithdraw or e(x)it: "
+            yield Console.ReadKey().KeyChar
+            Console.WriteLine() }
+    
+    let getAmount command =
+        Console.WriteLine()
+        Console.Write "Enter Amount: "
+        command, Console.ReadLine() |> Decimal.Parse
+
+
+module AppStart =
+
+    let withdrawWithAudit = auditAs "withdraw" composedLogger withdraw
+    let depositWithAudit = auditAs "deposit" composedLogger deposit
+    let loadAccountFromDisk = findTransactionsOnDisk >> loadAccount
+
+    [<EntryPoint>]
+    let main _ =
+        let openingAccount =
+            Console.Write "Please enter your name: "
+            Console.ReadLine() |> loadAccountFromDisk
+        
+        printfn "Current balance is £%M" openingAccount.Balance
+
+        let processCommand account (command, amount) =
+            printfn ""
+            let account =
+                if command = 'd' then account |> depositWithAudit amount
+                else account |> withdrawWithAudit amount
+            printfn "Current balance is £%M" account.Balance
+            account
+
+        let closingAccount =
+            commands
+            |> Seq.filter isValidCommand
+            |> Seq.takeWhile (not << isStopCommand)
+            |> Seq.map getAmount
+            |> Seq.fold processCommand openingAccount
+        
+        printfn ""
+        printfn "Closing Balance:\r\n %A" closingAccount
+        Console.ReadKey() |> ignore
+
+        0
