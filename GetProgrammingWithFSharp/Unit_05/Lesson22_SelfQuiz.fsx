f81748cb043dad06768f767b0ca032d99014f4bc
demirtasdev <demirtasdev@outlook.com>
2019-12-29 20:18:10 +0000

    Lesson 22 Self Quiz and Revision

---
+++ GetProgrammingWithFSharp/Unit_05/Lesson22_SelfQuiz.fsx
@@ -0,0 +1,26 @@
+open System
+
+// Q1: Declare an option type
+let myAge = Some 21
+
+// Q2: Explain the risk of using .Value on an option type
+// A2: When we use .Value, we are trying to get the value
+// without the foreknowledge that the value exists. This 
+// may throw an exception when done on an option of None.
+
+// Q3: Pattern Match against an option
+match myAge with
+| Some age -> printfn "Age stated: %d" age
+| None -> printfn "Age not stated."
+
+// Q4: Do an option mapping
+let something = myAge |> Option.map (fun x -> sprintf "%d" x)
+
+// Q5: Do an option binding
+let objectToOption option =
+    Some (sprintf "%A" option)
+
+let someOtherThing = myAge |> Option.bind objectToOption
+
+// Q6: Filter an option
+let myAgeFiltered = myAge |> Option.filter (fun x -> x > 24)
